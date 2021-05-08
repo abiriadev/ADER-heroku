56f8ed6e4154487955243f4e860213079534984c
abiria <abiria.dev@gmail.com>
2021-05-08 17:29:14 +0900

    edit for traking client ip address

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan')
+const chalk = require('chalk')
 
 const app = express();
 
 app.use(morgan('dev'))
+app.use((req, res, next) => {
+	const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+	console.log(chalk.redBright(`ip: ${ip}`));
+
+	next();
+})
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/add', (req, res) => {
@@ -13,7 +21,7 @@ app.get('/add', (req, res) => {
 	res.send(`<h1>result: ${parseInt(a) + parseInt(b)}</h1>`)
 })
 
-const port = 80 //58477
+const port = process.env.PORT || 80 //58477
 app.listen(port, () => {
-	console.log(`http://127.0.0.1:${port}`)
+	console.log(chalk.keyword('orange')(`server running at ${chalk.cyan(`http://127.0.0.1:${port}`)}`))
 })
